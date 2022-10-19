ee414edc9749c04fea1ddae800d7079ced6dac20
Leopoldo <poldosantiago@gmail.com>
2022-10-19 10:51:48 -0300

    opa

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ var app = express();
 
 app.use('/',express.static(__dirname + '/public'));
 
-app.listen(3000, () => console.log("escutando a porta 3000"));
+app.listen(process.env.PORT || 5000);
+// app.listen(3000, () => console.log("escutando a porta 3000"));
+
