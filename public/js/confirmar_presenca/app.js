08019db00e9d7362a58f91403377cfb3cb868ffe
Leopoldo <poldosantiago@gmail.com>
2022-10-20 02:06:18 -0300

    corrigido problema de não coletar os campos do carro

--- public/js/confirmar_presenca/app.js
+++ public/js/confirmar_presenca/app.js
@@ -7,12 +7,19 @@ let nomeCompleto = util.pegarParametroPagina("nome");
 document.querySelector('[data-tipo="nome"]').value = nomeCompleto;
 
 
-let inputs = document.querySelectorAll('[data-tipo]');
 //atribuindo comportamento ao campo quando perde o foco
+let inputs = document.querySelectorAll('[data-tipo]');
 inputs.forEach(input => input.addEventListener('blur', evento => validarCampoFormulario(evento.target)))
 
-let botao = document.querySelector('[data-botao]');
-botao.addEventListener('click', () => submeterFormulario(inputs));
 
+//botão para submeter o formulário
+let botao = document.querySelector('[data-botao]');
+botao.addEventListener('click', () => {
+	let inputsDeNovo = document.querySelectorAll('[data-tipo]'); //eu pego aqui de novo para incluir os campos do carro, caso seja acionado
+	submeterFormulario(inputsDeNovo);
+});
+
+
+//comportamento do radio buton - pergunta sobre o carro e etc
 let radios = document.querySelectorAll('[data-tipo="radio_carro"]'); //pega os elementos que contem a palavra 'radio_carro'
 radios.forEach(radio => radio.addEventListener('change', evento => validarCampoFormulario(evento.target)));
